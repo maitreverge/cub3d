0db0a19b749c0834f2e52e87a607a387dbed5f84
“florianverge” <verge.f@gmail.com>
2024-06-21 22:51:40 +0200

    init file for init

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,12 +6,21 @@
 /*   By: flverge <flverge@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 21:36:59 by flverge           #+#    #+#             */
-/*   Updated: 2024/06/21 21:38:30 by flverge          ###   ########.fr       */
+/*   Updated: 2024/06/21 22:45:59 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
+
+// Linking MLX header files
+#include "../MLX42/include/MLX42/MLX42.h"
+
+// Define libraries
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 
 // Window Size
 # define WIN_WIDTH 1000
