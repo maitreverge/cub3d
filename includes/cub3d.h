8c572ee9970475d1fd9330d3736b6ea5492b41d0
“florianverge” <verge.f@gmail.com>
2024-06-21 21:41:20 +0200

    rename `old_cub3d`

---
+++ includes/cub3d.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flverge <flverge@student.42perpignan.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/21 21:36:59 by flverge           #+#    #+#             */
+/*   Updated: 2024/06/21 21:38:30 by flverge          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+// Window Size
+# define WIN_WIDTH 1000
+# define WIN_HEIGHT 600
+
+# define RED	 "\033[0;31m"
+# define GREEN   "\033[0;32m"
+# define YELLOW  "\033[0;33m"
+# define BLUE	"\033[0;34m"
+# define MAGENTA "\033[0;35m"
+# define CYAN	"\033[0;36m"
+# define BOLD	"\033[1m"
+# define RESET   "\033[0m"
+
+#endif
